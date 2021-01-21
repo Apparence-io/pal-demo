c0955446fd074c9d418b4819c844b2317a912987
Gautier <gautier@apparence.io>
2021-01-21 11:40:58 +0100

    next

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pal/pal.dart';
 import 'package:pal_demo_app/routes.dart';
-import 'package:pal_demo_app/ui/pages/main_menu.dart';
 
-import 'ui/pages/home/home_page.dart';
-import 'ui/pages/movie/movie_page.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'ui/pages/profile/profile.dart';
 
 
 void main() {
@@ -18,23 +14,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: navigatorKey,
-      title: 'Pal Plugin Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        hintColor: Color(0xFFA3ABB3),
-        primaryColor: Color(0xFF111820),
-        accentColor: Color(0xFF268FCD),
-        backgroundColor: Color(0xFF111820),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: TextTheme(
-          bodyText1: GoogleFonts.poppins(fontSize: 32, color: Color(0xFFDFDFD5), height: 1.6),
-          bodyText2: TextStyle(),
+    return Pal(
+      editorModeEnabled: true,
+      appToken: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MThiZWYyOS1kODJkLTQ3Y2ItODQ2Yi1iZGY0Y2QyZmMyMzQiLCJ0eXBlIjoiUFJPSkVDVCIsImlhdCI6MTYwNjc0NzE2Nn0.8TgjVp9CvWNlTdKwzqsrbKk9mtPwbEoTTGhyZu7uZC4',
+      childApp: MaterialApp(
+        navigatorKey: navigatorKey,
+        title: 'Pal Plugin Demo',
+        navigatorObservers: [PalNavigatorObserver.instance()],
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          hintColor: Color(0xFFA3ABB3),
+          primaryColor: Color(0xFF111820),
+          accentColor: Color(0xFF268FCD),
+          backgroundColor: Color(0xFF111820),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: TextTheme(
+            bodyText1: GoogleFonts.poppins(fontSize: 32, color: Color(0xFFDFDFD5), height: 1.6),
+            bodyText2: TextStyle(),
+          ),
+          fontFamily: 'Poppins'
         ),
-        fontFamily: 'Poppins'
+        onGenerateRoute: routes,
       ),
-      onGenerateRoute: routes,
     );
   }
 }
