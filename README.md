582c8c636cccdd6384add1420c3f6bc70c14d9ad
Gautier <gautier@apparence.io>
2021-05-10 15:07:37 +0200

    readme

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 # pal_demo_app
-
-A flutter application using pal
+Demo application using Pal plugin 
+https://pal-plugin.tech
 
 ## Getting Started
+Start in editor mode using VScode launch mode: 
+'''[editor mode] start'''
 
-This project is a starting point for a Flutter application.
+Start in client mode using VScode launch mode: 
+'''[client mode] start'''
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main_getx.dart
+++ lib/main_getx.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:pal/pal.dart';
 import 'package:pal_demo_app/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-const bool PAL_LAUNCH_MODE = const bool.fromEnvironment("PAL_LAUNCH_MODE", defaultValue: true);
+import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(GetXMyApp());
+}
 
 const String APP_TOKEN =
     "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4ZWNkMjM5MC1kODExLTQxYzEtOWYxOS04ZWMyN2I0OTY0ZjAiLCJ0eXBlIjoiUFJPSkVDVCIsImlhdCI6MTYxNzM2NzAyMH0.c27i8hJ1QY9yzEx15yYKbn56qfIbPy58YvSia2M9VgE";
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class GetXMyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Pal(
-      editorModeEnabled: PAL_LAUNCH_MODE,
+    return Pal.fromAppBuilder(
+      navigatorKey: navigatorKey,
+      editorModeEnabled: false,
       appToken: APP_TOKEN,
-      childApp: MaterialApp(
+      childAppBuilder: (context) => GetMaterialApp(
         navigatorKey: navigatorKey,
         title: 'Pal Plugin Demo',
         navigatorObservers: [PalNavigatorObserver.instance()],
+        onGenerateRoute: routes,
         theme: ThemeData(
             primarySwatch: Colors.blue,
             hintColor: Color(0xFFA3ABB3),
@@ -35,7 +37,6 @@ class MyApp extends StatelessWidget {
               bodyText2: TextStyle(),
             ),
             fontFamily: 'Poppins'),
-        onGenerateRoute: routes,
       ),
     );
   }
